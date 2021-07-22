a9a6b3343f2c9a25949d6be77708e8d0a7f73cbe
liweiyap <liweiyap@gmail.com>
2021-07-22 21:17:37 +0800

    Add helper classes for proper resizing of BigImageView

---
+++ app/src/main/java/com/liweiyap/foxtrot/ui/image/SubsamplingScaleImageViewSizer.kt
@@ -0,0 +1,68 @@
+package com.liweiyap.foxtrot.ui.image
+
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
+
+class SubsamplingScaleImageViewSizer(private val ssiv: SubsamplingScaleImageView): SubsamplingScaleImageView.OnImageEventListener {
+
+    override fun onReady() {
+        ssiv.setDoubleTapZoomDuration(200)
+
+        var result = 0.5f
+        val imageWidth = ssiv.sWidth
+        val imageHeight = ssiv.sHeight
+        val viewWidth = ssiv.width
+        val viewHeight = ssiv.height
+
+        var hasZeroValue = false
+        if (imageWidth == 0 || imageHeight == 0 || viewWidth == 0 || viewHeight == 0) {
+            result = 0.5f
+            hasZeroValue = true
+        }
+
+        val viewWHRatio = viewWidth / viewHeight.toFloat()
+        val imageWHRatio = imageWidth / imageHeight.toFloat()
+
+        if (!hasZeroValue) {
+            result = if (imageWHRatio <= viewWHRatio) {
+                viewWidth / imageWidth.toFloat()
+            } else {
+                viewHeight / imageHeight.toFloat()
+            }
+        }
+
+        val maxScale = (viewWidth / imageWidth.toFloat()).coerceAtLeast(viewHeight / imageHeight.toFloat())
+        if (maxScale > 1) {
+            // image is smaller than screen, it should be zoomed out to its origin size
+            ssiv.minScale = 1f
+
+            // and it should be zoomed in to fill the screen
+            val defaultMaxScale = ssiv.maxScale
+            ssiv.maxScale = defaultMaxScale.coerceAtLeast(maxScale * 1.2f)
+
+            val fitScreenRatio = viewWHRatio / imageWHRatio
+
+            if (fitScreenRatio < 1.2 && fitScreenRatio > 0.9) {
+                result *= 2
+            }
+        } else {
+            // image is bigger than screen, it should be zoomed out to fit the screen
+            val minScale = (viewWidth / imageWidth.toFloat()).coerceAtMost(viewHeight / imageHeight.toFloat())
+            ssiv.minScale = minScale
+            // but no need to set max scale
+        }
+
+        // scale to fit screen, and center
+        ssiv.setDoubleTapZoomScale(result)
+        ssiv.resetScaleAndCenter()
+    }
+
+    override fun onImageLoaded() {}
+
+    override fun onPreviewLoadError(e: Exception?) {}
+
+    override fun onImageLoadError(e: Exception?) {}
+
+    override fun onTileLoadError(e: Exception?) {}
+
+    override fun onPreviewReleased() {}
+}
